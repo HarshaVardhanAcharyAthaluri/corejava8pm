74beb1f10ac707923e9e3e5b53c07c11570478d4
HarshaVardhan <athaluri.harsha@gmail.com>
2022-10-18 20:39:03 +0530

    if else demo

---
+++ myapp.java
@@ -0,0 +1,17 @@
+class myapp{
+	
+	
+	public static void main(String[] args){
+	
+		int age = 15;
+		
+		if(age>18){
+		System.out.println("Adult Eligible to caste your Vote and you can spoiil coountry happily");
+		}else{
+			int res = 18 - age;
+		System.out.println("Child Not Eligible to spoiil the country wait " + res + " more years");
+		}
+		
+		
+	}
+}
