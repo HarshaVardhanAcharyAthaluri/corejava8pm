04d727d0d8d02c59bca8906fb446b1fb617a3b54
HarshaVardhan <athaluri.harsha@gmail.com>
2022-10-19 20:45:35 +0530

    switch demo

---
+++ SwitchDemo.java
@@ -0,0 +1,20 @@
+class SwitchDemo{
+	
+		
+	public static void main(String[] args){
+	
+		int choice = 4;
+		
+		switch(choice){
+			case 1: System.out.println("First Choice");
+				break;
+			case 2: System.out.println("Second Choice");
+				break;
+			case 3: System.out.println("Third Choice");
+				break;
+			default: System.out.println(" Choice Over");
+
+		}
+
+	}
+}
